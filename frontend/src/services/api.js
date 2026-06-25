19765c3d4d430874c758a0405fe8627c409b98fc
sakshi89017 <km.sakshi.it.2023@miet.ac.in>
2026-06-25 13:44:47 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://commerce-api-ucbm.onrender.com/api'
+const BASE_URL = import.meta.env.VITE_API_URL // ✅ Now uses GitHub Actions env
 
 const api = axios.create({
   baseURL: BASE_URL,
